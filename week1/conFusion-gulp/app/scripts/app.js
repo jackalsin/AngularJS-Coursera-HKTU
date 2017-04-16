15bfa1cb95f6f6d64cbe08c80ff73980678b8e08
jackalsin <jackal.sin@hotmail.com>
2017-04-16 15:30:31 -0700

    finish Assignment 2

--- week1/conFusion-gulp/app/scripts/app.js
+++ week1/conFusion-gulp/app/scripts/app.js
@@ -76,10 +76,8 @@ angular.module('confusionApp', [])
         .controller('ContactController', ['$scope', function($scope) {
 
             $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-            
-            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-            
-            $scope.channels = channels;
+
+          $scope.channels = [{value: "tel", label: "Tel."}, {value: "Email", label: "Email"}];
             $scope.invalidChannelSelection = false;
                         
         }])
@@ -90,7 +88,7 @@ angular.module('confusionApp', [])
                 
                 console.log($scope.feedback);
                 
-                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
                 }
@@ -106,67 +104,68 @@ angular.module('confusionApp', [])
 
         .controller('DishDetailController', ['$scope', function($scope) {
 
-            var dish={
-                          name:'Uthapizza',
-                          image: 'images/uthapizza.png',
-                          category: 'mains', 
-                          label:'Hot',
-                          price:'4.99',
-                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                           comments: [
-                               {
-                                   rating:5,
-                                   comment:"Imagine all the eatables, living in conFusion!",
-                                   author:"John Lemon",
-                                   date:"2012-10-16T17:57:28.556094Z"
-                               },
-                               {
-                                   rating:4,
-                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                                   author:"Paul McVites",
-                                   date:"2014-09-05T17:57:28.556094Z"
-                               },
-                               {
-                                   rating:3,
-                                   comment:"Eat it, just eat it!",
-                                   author:"Michael Jaikishan",
-                                   date:"2015-02-13T17:57:28.556094Z"
-                               },
-                               {
-                                   rating:4,
-                                   comment:"Ultimate, Reaching for the stars!",
-                                   author:"Ringo Starry",
-                                   date:"2013-12-02T17:57:28.556094Z"
-                               },
-                               {
-                                   rating:2,
-                                   comment:"It's your birthday, we're gonna party!",
-                                   author:"25 Cent",
-                                   date:"2011-12-02T17:57:28.556094Z"
-                               }
-                               
-                           ]
-                    };
-            
-            $scope.dish = dish;
+          $scope.dish = {
+              name: 'Uthapizza',
+              image: 'images/uthapizza.png',
+              category: 'mains',
+              label: 'Hot',
+              price: '4.99',
+              description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+              comments: [
+                {
+                  rating: 5,
+                  comment: "Imagine all the eatables, living in conFusion!",
+                  author: "John Lemon",
+                  date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                  rating: 4,
+                  comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                  author: "Paul McVites",
+                  date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                  rating: 3,
+                  comment: "Eat it, just eat it!",
+                  author: "Michael Jaikishan",
+                  date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                  rating: 4,
+                  comment: "Ultimate, Reaching for the stars!",
+                  author: "Ringo Starry",
+                  date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                  rating: 2,
+                  comment: "It's your birthday, we're gonna party!",
+                  author: "25 Cent",
+                  date: "2011-12-02T17:57:28.556094Z"
+                }
+
+              ]
+            };
             
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
-            
+            var INIT_COMMENT = {'author':"", 'rating': 5, 'comment':"", date: new Date().toISOString()};
             //Step 1: Create a JavaScript object to hold the comment from the form
-            
+            $scope.curComment = JSON.parse(JSON.stringify(INIT_COMMENT));
+            $scope.updateDateListener = function () {
+              $scope.curComment.date = new Date().toISOString();
+            };
             $scope.submitComment = function () {
-                
-                //Step 2: This is how you record the date
-                "The date property of your JavaScript object holding the comment" = new Date().toISOString();
-                
+
+                $scope.updateDateListener();
                 // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push($scope.curComment);
                 
                 //Step 4: reset your form to pristine
-                
+                $scope.commentForm.$setPristine();
+
                 //Step 5: reset your JavaScript object that holds your comment
+                $scope.curComment = JSON.parse(JSON.stringify(INIT_COMMENT));
             }
         }])
 
